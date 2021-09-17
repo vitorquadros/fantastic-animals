931a5a72dc403fe67f41d02f8fa69c335b29f7df
Vitor Quadros <viitorquadros@hotmail.com>
2021-09-17 14:39:30 -0300

    feat (accordion list): verification if elements exist

--- script.js
+++ script.js
@@ -19,15 +19,24 @@ function initTabNav() {
   }
 }
 
-const accordionList = document.querySelectorAll('.js-accordion dt');
+function initAccordion() {
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+  const activeClass = 'active';
 
-function activeAccordion() {
-  this.classList.toggle('active');
-  this.nextElementSibling.classList.toggle('active');
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion);
+    });
+  }
 }
 
-accordionList.forEach((item) => {
-  item.addEventListener('click', activeAccordion);
-});
-
 initTabNav();
+initAccordion();
