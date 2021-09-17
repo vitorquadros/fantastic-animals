fee30f281b356b08e7b0f093187eaf576dda9c94
Vitor Quadros <viitorquadros@hotmail.com>
2021-09-17 14:14:12 -0300

    feat (tabmenu): verification if element exists before run script

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const tabMenu = document.querySelectorAll('.js-tabmenu li');
 const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-function activeTab(index) {
-  tabContent.forEach((section) => {
-    section.classList.remove('active');
+if (tabMenu.length && tabContent.length) {
+  tabContent[0].classList.add('active');
+
+  function activeTab(index) {
+    tabContent.forEach((section) => {
+      section.classList.remove('active');
+    });
+
+    tabContent[index].classList.add('active');
+  }
+
+  tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', () => activeTab(index));
   });
-
-  tabContent[index].classList.add('active');
 }
-
-tabMenu.forEach((itemMenu, index) => {
-  itemMenu.addEventListener('click', () => activeTab(index));
-});
