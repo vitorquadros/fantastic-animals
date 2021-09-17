06d8da4dec8fc9e3640cfe266b19208bc2a81a63
Vitor Quadros <viitorquadros@hotmail.com>
2021-09-17 14:07:37 -0300

    feat: create script tabmenu

---
+++ script.js
@@ -0,0 +1,14 @@
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+function activeTab(index) {
+  tabContent.forEach((section) => {
+    section.classList.remove('active');
+  });
+
+  tabContent[index].classList.add('active');
+}
+
+tabMenu.forEach((itemMenu, index) => {
+  itemMenu.addEventListener('click', () => activeTab(index));
+});
