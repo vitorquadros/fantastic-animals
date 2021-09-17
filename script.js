c9f8c0cbe6ede55a7ce38c3da59db0df631f9dc1
Vitor Quadros <viitorquadros@hotmail.com>
2021-09-17 14:31:02 -0300

    feat: create accordion list script

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+function initTabNav() {
+  const tabMenu = document.querySelectorAll('.js-tabmenu li');
+  const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-if (tabMenu.length && tabContent.length) {
-  tabContent[0].classList.add('active');
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add('active');
 
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove('active');
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove('active');
+      });
+
+      tabContent[index].classList.add('active');
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => activeTab(index));
     });
-
-    tabContent[index].classList.add('active');
   }
-
-  tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => activeTab(index));
-  });
 }
+
+const accordionList = document.querySelectorAll('.js-accordion dt');
+
+function activeAccordion() {
+  this.classList.toggle('active');
+  this.nextElementSibling.classList.toggle('active');
+}
+
+accordionList.forEach((item) => {
+  item.addEventListener('click', activeAccordion);
+});
+
+initTabNav();
