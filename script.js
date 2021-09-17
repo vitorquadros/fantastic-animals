6eee0e09d75c4d3d7917dda3294a56a9d8005c65
Vitor Quadros <viitorquadros@hotmail.com>
2021-09-17 14:54:26 -0300

    feat: create smooth scroll script

--- script.js
+++ script.js
@@ -38,5 +38,32 @@ function initAccordion() {
   }
 }
 
+function initSmoothScroll() {
+  const links = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+
+    //  alternative way
+    // const top = section.offsetTop;
+    // window.scrollTo({
+    //   top,
+    //   behavior: 'smooth',
+    // });
+  }
+
+  links.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+}
+
 initTabNav();
 initAccordion();
+initSmoothScroll();
